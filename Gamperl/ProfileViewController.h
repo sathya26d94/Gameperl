d92446c8925944433818055dd433a96eb90a3b0b
sathya26d <sathiyamoorthy@theviralfever.com>
2017-07-17 19:31:15 +0530

    todays changes

--- Gamperl/ProfileViewController.h
+++ Gamperl/ProfileViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ProfileViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
 
 @end
